c33574f85f36d36837b94246abcae318f3310f82
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-02-25 21:11:06 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from __future__ import with_statement
-import sys
 import setuptools
 
 
@@ -18,7 +17,12 @@ setuptools.setup(
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/DateTimeRange",
-    description="Python library for time of range.",
+    description="""
+    Python library to handle the routine work associated with the time range,
+    such as test whether a time is within the time range,
+    get time intersection, truncating time etc.
+    """,
+    keywords="date time range",
     long_description=long_description,
     license="MIT License",
     include_package_data=True,
