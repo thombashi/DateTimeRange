b84522ca578865e258f3f3d1948d95a6debe4f14
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-04-23 22:27:11 +0900

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="DateTimeRange",
-    version="0.3.1",
+    version="0.3.2",
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/DateTimeRange",
