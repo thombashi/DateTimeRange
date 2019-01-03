2b4ffd2835764bd703c593d1545d71f02ddb84b2
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-01-03 11:13:45 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setuptools.setup(
     extras_require={
         "build": ["wheel"],
         "docs": docs_requires,
-        "release": ["releasecmd>=0.0.12"],
+        "release": ["releasecmd>=0.0.12,<0.1.0"],
         "test": tests_requires,
     },
 
